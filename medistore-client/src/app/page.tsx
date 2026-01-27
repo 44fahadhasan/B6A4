76c0098751f4725aa5d649473676c9fb09f126a1
44fahadhasan <44fahadhasan@gmail.com>
2026-01-27 13:41:08 +0600

    add: dark mode

--- medistore-client/src/app/page.tsx
+++ medistore-client/src/app/page.tsx
@@ -1,5 +1,11 @@
 import { ComponentExample } from "@/components/component-example";
+import { ModeToggle } from "@/components/layout/mode-toggle";
 
 export default function Page() {
-return <ComponentExample />;
+  return (
+    <div>
+      <ModeToggle />
+      <ComponentExample />
+    </div>
+  );
 }
