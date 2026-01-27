9b256df0e107575aa6e902e5551800f37fa241af
44fahadhasan <44fahadhasan@gmail.com>
2026-01-27 12:59:11 +0600

    remove: test code from app.ts file

--- medistore-server/src/app.ts
+++ medistore-server/src/app.ts
@@ -1,23 +1,10 @@
 import express from "express";
 import config from "./config";
-import { prisma } from "./config/prisma";
 
 const app = express();
 
 app.get("/", (_, res) => {
   res.send(`Welcome to the ${config.site_name}`);
 });
-
-const createUser = async () => {
-  const result = await prisma.user.create({
-    data: {
-      name: "Fahad Hasan",
-      email: "fahadhasan@gmail.com",
-    },
-  });
-  console.log({ result });
-};
-
-await createUser();
 
 export default app;
