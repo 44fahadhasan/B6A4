ebed0b7cc563489255cb56749ef658613f3fc05a
44fahadhasan <44fahadhasan@gmail.com>
2026-02-10 14:44:30 +0600

    update

---
+++ medistore-client/src/app/(public-layout)/medicines/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex min-h-[calc(100vh-305px)] w-full items-center justify-center text-center">
+      <p className="text-sm text-muted-foreground">Loading, please wait…</p>
+    </div>
+  );
+}
